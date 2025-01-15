888c664cd27fd24113cc3463b1c5ccde7b1a7d54
dashka-bat <dushkab02@gmail.com>
2025-01-15 18:39:46 +0800

    commit3

--- index.ts
+++ index.ts
@@ -3,11 +3,11 @@ import { configDotenv } from "dotenv";
 import { MongoClient } from "mongodb";
 import { error } from "console";
 import mongoose from "mongoose";
- const cors= require(`cors`)
-// const mongoose = require("mongoose");
+import { food_categoryrouter } from "./router/food-category";
+const cors = require(`cors`);
 const PORT = 8000;
 const app = express();
-app.use(cors())
+app.use(cors());
 // const URL =
 //   "mongodb+srv://ADMIN:dashka0318@cluster1.xrs1i.mongodb.net/?retryWrites=true&w=majority&appName=cluster1";
 configDotenv();
@@ -30,48 +30,8 @@ async function connectMongoDB() {
 
 connectMongoDB();
 
-const food_Category_Schema = new mongoose.Schema(
-  {
-    categoryName: String,
-  },
-  { timestamps: true }
-);
-const FoodCategoryModel = mongoose.model(
-  "category",
-  food_Category_Schema,
-  "food-category"
-);
-app.get("/food-category/", async (req: Request, res: Response) => {
-  const foodname = await FoodCategoryModel.find();
-  console.log(foodname);
-  res.json(foodname);
-});
-app.get("/food-category/:id", async (req: Request, res: Response) => {
-  const id=req.params.id
-  const foodname = await FoodCategoryModel.findById(id);
-  console.log(foodname);
-  res.json(foodname);
-});
-app.post("/food-category/", async (req: Request, res: Response) => {
-  const name = req.body.categoryName;
-  console.log("__________________", name);
-  const foodname = await FoodCategoryModel.create({ categoryname:name  });
-  console.log(foodname);
-  res.send("done");
-});
-app.delete("/food-category:id", async (req: Request, res: Response) => {
-  const foodname = await FoodCategoryModel.findByIdAndDelete(req.params.id);
-  console.log(foodname);
-  res.send("deleted");
-});
-app.put("/food-category:id", async (req: Request, res: Response) => {
-  const uptadeName=req.body.categoryName
-  const foodname = await FoodCategoryModel.findByIdAndUpdate(req.params.id, {
-    categoryName: uptadeName
-  },{new:true});
-  console.log(foodname);
-  res.send("uptaded");
-});
+app.use("/food-category/", food_categoryrouter);
+
 app.listen(PORT, () => {
   console.log(`port${PORT}`);
 });
